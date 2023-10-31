362497eff1b53703883990d5a9a6e71887d0a9fb
nazmul <nazmulhussain.api@gmail.com>
2023-10-31 14:01:50 +0600

    Format

--- src/components/TB-status/TB-status.jsx
+++ src/components/TB-status/TB-status.jsx
@@ -14,161 +14,152 @@ import ExaminationFinding from "../StationFourA/Illness/ExaminationFinding";
 import TBPastHistory from "../StationFourA/Illness/TBPastHistory";
 
 const TbStatus = () => {
-   const userData = loggedInUserData();
-   const user = userData.name;
-   // console.log(user);
+  const userData = loggedInUserData();
+  const user = userData.name;
+  // console.log(user);
 
-   const { patient } = useSelector((state) => state.patients);
+  const { patient } = useSelector((state) => state.patients);
 
-   const [PatientId] = useState(patient?.PatientId);
-   const [formData, setFormData] = useState({
-      ProvisionalDiagnosis: [],
-      LabInvestigation: [],
-      TreatmentSuggestion: [],
-      Referral: [],
-      Advice: [],
-      FollowUpDate: [
-         {
-            PatientId: PatientId,
-            followUpDate: "",
-            comment: "",
-            Status: "",
-            CreateUser: user,
-            OrgId: "73CA453C-5F08-4BE7-A8B8-A2FDDA006A2B",
-         },
-      ],
-   });
+  const [PatientId] = useState(patient?.PatientId);
+  const [formData, setFormData] = useState({
+    ProvisionalDiagnosis: [],
+    LabInvestigation: [],
+    TreatmentSuggestion: [],
+    Referral: [],
+    Advice: [],
+    FollowUpDate: [
+      {
+        PatientId: PatientId,
+        followUpDate: "",
+        comment: "",
+        Status: "",
+        CreateUser: user,
+        OrgId: "73CA453C-5F08-4BE7-A8B8-A2FDDA006A2B",
+      },
+    ],
+  });
 
-   return (
-      <>
-         <section>
-            <SectionBanner title={`TB Status - ${patient?.GivenName}`} />
-            <div className="container">
-               <div className="row justify-content-center">
-                  <div className="col-lg-6">
-                     <div className="show-data mb-4">
-                        <PatientShortInfo />
-                     </div>
-                  </div>
-               </div>
+  return (
+    <>
+      <section>
+        <SectionBanner title={`TB Status - ${patient?.GivenName}`} />
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-6">
+              <div className="show-data mb-4">
+                <PatientShortInfo />
+              </div>
+            </div>
+          </div>
 
-               <form className="mt-3">
-                  <div className="row d-flex justify-content-center">
-                     <div className="col-lg-6">
-                        <h6>
-                           Additional History for Suspected TB Cases (PTB only)
-                        </h6>
+          <form className="mt-3">
+            <div className="row d-flex justify-content-center">
+              <div className="col-lg-6">
+                <h6>Additional History for Suspected TB Cases (PTB only)</h6>
 
-                        <Accordion>
-                           <Accordion.Item
-                              eventKey="1"
-                              className="input-shadow mb-3 rounded"
-                           >
-                              <Accordion.Header>
-                                 Additional Symptoms
-                              </Accordion.Header>
-                              <Accordion.Body>
-                                 <div className="">
-                                    <div className="">
-                                       <p className="font-16 fw-semibold">
-                                          Do you have any of the following
-                                          Symptoms?
-                                       </p>
-                                    </div>
-                                    <div className="position-relative">
-                                       <AdditionalSymptoms
-                                          className="toggle-btn"
-                                          formData={formData}
-                                          setFormData={setFormData}
-                                       />
-                                    </div>
-                                 </div>
-                              </Accordion.Body>
-                           </Accordion.Item>
+                <Accordion>
+                  <Accordion.Item
+                    eventKey="1"
+                    className="input-shadow mb-3 rounded"
+                  >
+                    <Accordion.Header>Additional Symptoms</Accordion.Header>
+                    <Accordion.Body>
+                      <div className="">
+                        <div className="">
+                          <p className="font-16 fw-semibold">
+                            Do you have any of the following Symptoms?
+                          </p>
+                        </div>
+                        <div className="position-relative">
+                          <AdditionalSymptoms
+                            className="toggle-btn"
+                            formData={formData}
+                            setFormData={setFormData}
+                          />
+                        </div>
+                      </div>
+                    </Accordion.Body>
+                  </Accordion.Item>
 
-                           <Accordion.Item
-                              eventKey="2"
-                              className="input-shadow mb-3 rounded"
-                           >
-                              <Accordion.Header>
-                                 Examination Finding: Please Auscultate Lungs
-                              </Accordion.Header>
-                              <Accordion.Body>
-                                 <div className="">
-                                    <div className="">
-                                       <p className="font-16 fw-semibold">
-                                          Do you have any of the following
-                                          Symptoms?
-                                       </p>
-                                    </div>
-                                    <div className="position-relative">
-                                       <ExaminationFinding
-                                          className="toggle-btn"
-                                          formData={formData}
-                                          setFormData={setFormData}
-                                       />
-                                    </div>
-                                 </div>
-                              </Accordion.Body>
-                           </Accordion.Item>
+                  <Accordion.Item
+                    eventKey="2"
+                    className="input-shadow mb-3 rounded"
+                  >
+                    <Accordion.Header>
+                      Examination Finding: Please Auscultate Lungs
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <div className="">
+                        <div className="">
+                          <p className="font-16 fw-semibold">
+                            Do you have any of the following Symptoms?
+                          </p>
+                        </div>
+                        <div className="position-relative">
+                          <ExaminationFinding
+                            className="toggle-btn"
+                            formData={formData}
+                            setFormData={setFormData}
+                          />
+                        </div>
+                      </div>
+                    </Accordion.Body>
+                  </Accordion.Item>
 
-                           <Accordion.Item
-                              eventKey="3"
-                              className="input-shadow mb-3 rounded"
-                           >
-                              <Accordion.Header>
-                                 TB Past History
-                              </Accordion.Header>
-                              <Accordion.Body>
-                                 <div className="">
-                                    <div className="">
-                                       <p className="font-16 fw-semibold">
-                                          Do you have Past TB History?
-                                       </p>
-                                    </div>
-                                    <div className="position-relative">
-                                       <TBPastHistory
-                                          className="toggle-btn"
-                                          formData={formData}
-                                          setFormData={setFormData}
-                                       />
-                                    </div>
-                                 </div>
-                              </Accordion.Body>
-                           </Accordion.Item>
-                        </Accordion>
-                     </div>
-                  </div>
+                  <Accordion.Item
+                    eventKey="3"
+                    className="input-shadow mb-3 rounded"
+                  >
+                    <Accordion.Header>TB Past History</Accordion.Header>
+                    <Accordion.Body>
+                      <div className="">
+                        <div className="">
+                          <p className="font-16 fw-semibold">
+                            Do you have Past TB History?
+                          </p>
+                        </div>
+                        <div className="position-relative">
+                          <TBPastHistory
+                            className="toggle-btn"
+                            formData={formData}
+                            setFormData={setFormData}
+                          />
+                        </div>
+                      </div>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </div>
+            </div>
 
-                  <div className="text-center mt-3 position-relative">
-                     <Button
-                        className="border-0 button-color text-white py-2 px-3 text-capitalize rounded	undefined"
-                        type="submit"
-                     >
-                        Save & Next
-                     </Button>
-                     {/* <SingleButton
+            <div className="text-center mt-3 position-relative">
+              <Button
+                className="border-0 button-color text-white py-2 px-3 text-capitalize rounded	undefined"
+                type="submit"
+              >
+                Save & Next
+              </Button>
+              {/* <SingleButton
                 btnOne="save & Next"
                 link="/cardiovascular-risk-nonlab"
               /> */}
 
-                     <div className="previewBtn">
-                        <Link
-                           to="/prescription"
-                           className="border-0 button-color text-white py-2 px-3 text-capitalize rounded"
-                        >
-                           Histrory
-                        </Link>
-                     </div>
-                  </div>
-               </form>
-
+              <div className="previewBtn">
+                <Link
+                  to="/prescription"
+                  className="border-0 button-color text-white py-2 px-3 text-capitalize rounded"
+                >
+                  Histrory
+                </Link>
+              </div>
             </div>
+          </form>
+        </div>
 
-            <GlobalButton />
-         </section>
-      </>
-   );
+        <GlobalButton />
+      </section>
+    </>
+  );
 };
 
 export default TbStatus;
